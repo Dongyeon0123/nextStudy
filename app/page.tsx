10f0cf739f30359a2093b9e60211724510cc0cbc
dongyeon <ldy1951123@gmail.com>
2025-04-23 04:06:59 +0900

    TypeScript & NextJS 시작

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  let name :string = 'lee'
+  let age :number = 25
+  let link :string = 'http://google.com'
+
   return (
-    <div>
-      
+    <div className="App">
+      <h4 className="title">애플후레시 { age }</h4>
+      <p className="title-sub">by dev { name }</p>
+      <a href={ link }>링크</a>
     </div>
   );
 }
