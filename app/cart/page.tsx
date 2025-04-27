5f7cc3e2189b65649440c27a97d3542a991c6f5f
dongyeon <ldy1951123@gmail.com>
2025-04-27 23:08:44 +0900

    next.js props사용법

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,6 +3,7 @@ import {age, name} from './data'
 import Hello from './hello'
 
 export default function Cart() :JSX.Element {
+    let 장바구니 :string[] = ['Tomato', 'Pasta', 'Coconut']
     return (
       <div>
         <h4 className="title">Cart</h4>
@@ -11,18 +12,24 @@ export default function Cart() :JSX.Element {
           <p>$40 {name}</p>
           <p>1개</p>
         </div>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
+        <CartItem 작명={장바구니[0]}/>
+        <CartItem 작명={장바구니[1]}/>
+        <CartItem 작명={장바구니[2]}/>
         <Hello/>
+        <Banner content="롯데카드"/>
+        <Banner content="현대카드"/>
       </div>
     )
 }
 
-function CartItem() :JSX.Element {
+function Banner(props: {content :string}) :JSX.Element {
+    return <h4>{props.content} 결제 행사중</h4>
+}
+
+function CartItem(props: {작명 :string}) :JSX.Element {
     return (
         <div className="cart-item">
-          <p>상품명</p>
+          <p>{props.작명}</p>
           <p>$40</p>
           <p>1개</p>
         </div>
